dc3ad7d90c0dbf35dcf650a50f4aa9a23092ac16
LEM1K <76048133+LEM1K@users.noreply.github.com>
2021-07-19 14:56:27 +0900

    commit1

---
+++ Algorithm STUDY/src/week1/Continuous_sum.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Continuous_sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Algorithm STUDY/src/week1/Sum_of_Subsequence.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class Sum_of_Subsequence {
+	private static int[] number;
+	private static int count = 0;
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
