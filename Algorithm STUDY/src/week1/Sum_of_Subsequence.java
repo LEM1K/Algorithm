272509da9f73c123c754d88b3e5d06efdbd740bf
LEM1K <76048133+LEM1K@users.noreply.github.com>
2021-07-19 19:59:20 +0900

    부분 수열의 합 알고리즘

--- Algorithm STUDY/src/week1/Sum_of_Subsequence.java
+++ Algorithm STUDY/src/week1/Sum_of_Subsequence.java
@@ -1,13 +1,43 @@
 package week1;
 
-public class Sum_of_Subsequence {
-	private static int[] number;
-	private static int count = 0;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+/*백준1182 - 부분 수열의 합
+ *version 1
+ */
+
+public class Sum_of_Subsequence {
+	static int[] arr;
+	static int n, s, cnt = 0;
+	
+	private static void dfs(int v, int su) {
+		if(v == n) {
+			if(su == s) cnt++;
+			return;
+		}
+
+		dfs(v + 1, su + arr[v]);
+		dfs(v + 1, su);
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		n = input.nextInt();
+		//배열 사이즈
+		s = input.nextInt();
+		//결과 값
+		arr = new int[n];
 		
+		for(int i = 0; i < n; i++) {
+			arr[i] = input.nextInt();
+		}
+		Arrays.sort(arr);
+		//배열 정렬
+		
+		dfs(0, 0);
+		System.out.print(s == 0? --cnt : cnt);
 	}
-
 }
+
